08bfa062c3c002e66b3f5dd40b476bc7c42ba4c3
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-02-18 16:30:40 +0530

    Update UpperAnnouncement.jsx

--- src/components/UpperAnnouncement.jsx
+++ src/components/UpperAnnouncement.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styled,{keyframes} from 'styled-components'
 
 const colorChange=keyframes`
- 0%   {background-color: #A675A1;}
-    50%  {background-color: #8F3985;}
-    100% {background-color: #A675A1;}
+      0%  {background-color: #a68e75;}
+     50% {background-color: #93c90b;}
+    100% {background-color: #a68e75;}
 `
 
 const Container=styled.div`
@@ -21,7 +21,7 @@ color: white;
 export default function UpperAnnouncement() {
     return (
         <Container>
-           <div style={{marginTop:"10px"}}> <p>DIWALI SALE!!! upto 50% OFF</p></div>
+           <div style={{marginTop:"10px"}}> <p>GREAT INDIAN SALE!!! upto 60% OFF </p></div>
         </Container>
     )
 }
