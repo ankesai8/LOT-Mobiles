259b4679dff5a06a02fb6c6c59eb3d6bdead69a0
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-02-18 16:34:39 +0530

    Update LowerAnnouncement.jsx

--- src/components/LowerAnnouncement.jsx
+++ src/components/LowerAnnouncement.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import styled,{keyframes} from 'styled-components'
 import { small } from '../responsive'
+
 const runningText =keyframes`
 0% {transform:translateX(100%)}
 100%{transform:translateX(-100%)}
+`
+const colorChange=keyframes`
+      0%  {background-color: #a68e75;}
+     50% {background-color: #93c90b;}
+    100% {background-color: #a68e75;}
 `
 
 const Container=styled.div`
 height: 2.5rem;
 font-weight: 700;
 color: black;
-background-color: #CEA2AC;
+background-color: #93c90b;
 overflow: hidden;
 ${small({height:"4rem"})}
 &  p{
 animation: ${runningText} 15s infinite linear;
+animation: ${colorChange} 15s infinite linear;
 width: 100%;
 position: relative;
 top: -5px;
