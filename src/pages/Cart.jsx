18f70b1e7ed3e6ac3a24f3df9e404347b549ed20
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-02-19 11:23:23 +0530

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+// import { Add, Remove } from '@material-ui/icons'
 import React, { useState,useEffect } from 'react'
 import styled from 'styled-components'
 import CartItem from '../components/CartItem'
@@ -12,8 +14,10 @@ import {Link} from "react-router-dom"
 import StripeCheckout from 'react-stripe-checkout';
 import { useHistory } from 'react-router'
 import { ToastContainer,Toast } from 'react-bootstrap'
+// import { userRequest } from '../axiosMethods'
 import axios from 'axios'
 
+// const KEY=process.env.REACT_APP_STRIPE
 
 const Maincontainer=styled.div`
 background-color:whitesmoke;
@@ -126,7 +130,7 @@ export default function Cart() {
                    <Link to="/products"> <Button>Continue To Shop</Button></Link>
                 </div>
                 <div>
-                    <h1>Your Cart</h1>
+                    <h1>Your Bag</h1>
                 </div>
                 <div>
                
@@ -159,11 +163,11 @@ export default function Cart() {
                     <div>
                         {user.currentUser ?
                         <StripeCheckout
-                        name="Shop-Cart"
-                        image="https://in.images.search.yahoo.com/images/view;_ylt=AwrwS2DRgBBizBMANhwO9olQ;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzkwOWQ5NGM4MjI5MTY4YWNjYzZjNTEzOTlhMzc4OTdhBGdwb3MDODMEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fyhs%2Fsearch%3Fp%3Dshop%2Bcart%2Blogo%26ei%3DUTF-8%26type%3D87infr0srvimo2001120%26fr%3Dyhs-omr-001%26hsimp%3Dyhs-001%26hspart%3Domr%26param1%3Dy6bdVFVIsvuYsgEClQfz8I7gEdb7af9GSy3VsrdP%252BKIXhco7CZqVDwZ2NxTWPZsilmfYcLnBIe9wEP0Es3UUq%252BTvBofTWFOrVFYGN%252Fm%252B5%252B39k7Ju1zTYub3PzAar5yJ8cxouzoUBePzpqxoGMk9FXThZUN%252FHIutOepldfuBn0OGwGs9y0I6YjGq7bwom4Qz5ll2tjYGlTxX7RHmBrzpXVsLAW0Hu7oiQOPpbRYEPDgihojHoxlrYoKSelxwsj6B65WDIlgGCixSnxDq9W8PnI1gIt2WOqQ%252BXJALrWfuAeKTCYzfbZVAn1C3njQybogm2mwj14mtrvA2t46spT%252Bv9YGWV8cMiQqTvygiOFWe5in2Cezo2%252F8uNE5QRaTSiQJ0hxwQ4Kd3vA%252F0%252FKKU3Lljing%253D%253D%26nost%3D1%26tab%3Dorganic%26ri%3D83&w=900&h=940&imgurl=www.clipartkey.com%2Fmpngs%2Fm%2F25-256300_circle-shopping-cart-logo.png&rurl=https%3A%2F%2Fwww.clipartkey.com%2Fview%2FixToTb_circle-shopping-cart-logo%2F&size=63.0KB&p=shop+cart+logo&oid=909d94c8229168accc6c51399a37897a&fr2=&fr=yhs-omr-001&tt=Circle+Shopping+Cart+Logo+%2C+Free+Transparent+Clipart+-+ClipartKey&b=61&ni=120&no=83&ts=&tab=organic&sigr=7s.51yCCrwoP&sigb=ZeRm2SNLfFNJ&sigi=fWbRdt01ABpw&sigt=Y1N0uEsfyuN1&.crumb=4YTQVgG.te0&fr=yhs-omr-001&hsimp=yhs-001&hspart=omr&type=87infr0srvimo2001120&param1=y6bdVFVIsvuYsgEClQfz8I7gEdb7af9GSy3VsrdP%2BKIXhco7CZqVDwZ2NxTWPZsilmfYcLnBIe9wEP0Es3UUq%2BTvBofTWFOrVFYGN%2Fm%2B5%2B39k7Ju1zTYub3PzAar5yJ8cxouzoUBePzpqxoGMk9FXThZUN%2FHIutOepldfuBn0OGwGs9y0I6YjGq7bwom4Qz5ll2tjYGlTxX7RHmBrzpXVsLAW0Hu7oiQOPpbRYEPDgihojHoxlrYoKSelxwsj6B65WDIlgGCixSnxDq9W8PnI1gIt2WOqQ%2BXJALrWfuAeKTCYzfbZVAn1C3njQybogm2mwj14mtrvA2t46spT%2Bv9YGWV8cMiQqTvygiOFWe5in2Cezo2%2F8uNE5QRaTSiQJ0hxwQ4Kd3vA%2F0%2FKKU3Lljing%3D%3D"
+                        name="ShopCArt"
+                        image="https://i.pinimg.com/736x/5d/26/a1/5d26a173f443cbd190e34481438d474b.jpg"
                         billingAddress
                         shippingAddress
-                        description={`Your  Total Cart is $ ${cart.total}`}
+                        description={`Your Cart Total is $ ${cart.total}`}
                         amount={cart.total*100}
                         token={onToken}
                         stripeKey="pk_test_51JpnsFSJXTWe5Zf11N05XLzTKIGjBGK5H030E43f2cOWiJnaGM6fXqHt7FMEqLEs6BqbjZosPSLxGWgblW8V04CU00CSqFgR2n">
@@ -175,13 +179,13 @@ export default function Cart() {
                         <Button onClick={()=>{setAlert(true)}}>checkOut</Button>
                         </>}</div>
                         <div style={{color:"yellow green"}}>
-                            <p>credentials to checkout:</p><br>
-                            <p>card number: <b>4242424242424242</b></p><br>
-                            <p>Expiry:<b>02/22</b></p><br>                  
-                            <p>CVV: <b>222</b></p><br>
+                            <p>Checkout Demo credentials</p>
+                            <p>card number: <b>4242424242424242</b></p>
+                            <p>Expiry:<b> 02/22 </b></p>                   
+                            <p>CVV: <b>222 r</b></p>
                         </div>
                 </SummaryContainer>
-              </>:<><h2  style={{textAlign:"center" ,width:"100%"}}>Your Cart is empty</h2></>}
+              </>:<><h2  style={{textAlign:"center" ,width:"100%"}}>Your Crart is empty</h2></>}
             </Container>
 
             <Newsletter/>
